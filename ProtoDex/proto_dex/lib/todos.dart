a437d26ecb354180e960e99af04de2ed7215ffb5
Icaro Torres <icaroto@outlook.com>
2023-11-02 20:47:11 +0000

    new pokedex fixes

--- ProtoDex/proto_dex/lib/todos.dart
+++ ProtoDex/proto_dex/lib/todos.dart
@@ -3,17 +3,17 @@ class Todos {
 /*
 
 TODO: Search return Form Name instead of main name (work arounds?)
+
 TODO: Image enhacement
   Types, Backgrounds on app
-TODO: Archibac issue on LF/FT when screen size small
 TODO: Check permission for windows desktop
+TODO BUG iOS: Archibac issue on LF/FT when screen size small
 TODO BUG iOS: Screenshot doesnt work on ios simulator
 TODO BUG iOS: Permission issue with mac desktop
 TODO BUG iOS: Back button on details screen overlaps with pokemon name
 TODO BUG iOS: List to have a back button on iOS
 TODO BUG iOS: Details page remove static height/width as it breaks on iOS
 TODO BUG iOS: Check why image looks smaller on iOS????
-TODO: On Living dex or any list tile of type Expand, keep it opened after ticking one
 
 TODO: Add generation property + generation filter (use to test the auto updates)
 TODO: Tracker is displaying forms that are not really catchable (eg. Mimiko hunger form, Ogerpon masks)
